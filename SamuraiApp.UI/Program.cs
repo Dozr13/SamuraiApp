f0bff128ea698e361bd1f02682d7b319a5a0ca87
Wade Pate <wade.pate@intimetec.com>
2021-07-19 16:12:05 -0600

    added logging to ef core's workload

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
 
 namespace SamuraiApp.UI
 {
     class Program
     {
-        static void Main(string[] args)
+        public static SamuraiContext _context = new SamuraiContext();
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AddSamurais("Wade", "Kota");
+            GetSamurais();
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurais(params string[] names)
+        {
+            foreach (string name in names)
+            {
+                _context.Samurais.Add(new Samurai { Name = name });
+            }
+            _context.SaveChanges();
+        }
+        private static void GetSamurais()
+        {
+            var samurais = _context.Samurais
+                .TagWith("ConsoleApp.Program.GetSamurais method")
+                .ToList();
+            Console.WriteLine($"Samurai count is {samurais.Count}");
+            foreach (var samurai in samurais)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
